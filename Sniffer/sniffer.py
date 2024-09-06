5a02338e62dabab2d6352ea7a4d6434b3f6311b2
LorenzoTerlizzi <lorenzoterlizzi2004@gmail.com>
2024-09-06 12:24:11 +0200

    cambio

--- Sniffer/sniffer.py
+++ Sniffer/sniffer.py
@@ -2,9 +2,13 @@ from scapy.all import *
 
 iPkt = 0
 
-def process_pocket(pkt):
+def process_pocket(packet):
     global iPkt
     iPkt += 1
-    print("Ho letto un pacchetto sulla tua macchina" + str(iPkt))
+    print("Ho letto un pacchetto sulla tua macchina " + str(iPkt))
+
+    if not packet.haslayer(IP):
+        return
 
 sniff(iface = "enp4s0", filter = "tcp", prn = process_pocket)
+
