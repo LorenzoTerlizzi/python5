e8040817ecf7b6aa06973bdd82179977741facfd
LorenzoTerlizzi <lorenzoterlizzi2004@gmail.com>
2024-09-08 18:35:21 +0200

    modifica

--- CercaStringa/install.sh
+++ CercaStringa/install.sh
@@ -1,1 +1,7 @@
-ls -l
+mkdir myProject
+my cercaStringa.py
+my requirements.txt cd myProject
+python3 -m venv myvenv
+source myvenv/bin/activate
+pip install -r requirements.txt
+python cercaStringa.py
